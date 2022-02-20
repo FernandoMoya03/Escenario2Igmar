3a58112ed746b9497a4db3f86d7b54c44baeceec
Fernando Moya <fernando03082000@gmail.com>
2022-02-20 15:22:49 -0600

    cambios

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Registro</title>
-  <link rel="icon" href="{{ asset('images/usuario.jpg') }}">
+  <link rel="icon" href="{{ asset('images/equis.png') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="estilo.css">
 </head>
--- resources/views/verificacion.blade.php
+++ resources/views/verificacion.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
   <meta charset="utf-8">
-  <title>VERIFICACION</title>
+  <title>Verificacion</title>
   <link rel="icon" href="{{ asset('images/verificacion.jpg') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="estilo.css">
